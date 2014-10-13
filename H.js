0507806573834256897233db20fe86e0f1ada555
Jascha Ehrenreich <jascha@jaeh.at>
2014-10-13 03:56:07 +0200

    removed unneeded hosts var

--- H.js
+++ H.js
@@ -1,19 +1,5 @@
 'use strict';
-
 var express = require('express')
   , H = express()
-  , hosts = {
-      development: [
-        'jaeh.test'
-      , 'jaeh.t'
-      ]
-    , production: [
-        'jaeh.at'
-      ]
-  }
 ;
-
-//this is the only needed setting
-H.set( 'hosts', hosts[H.get('env')] );
-
 module.exports = H;
