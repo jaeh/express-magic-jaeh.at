5d25853f4db9d450ff60e57bd7d2eb255c2b24f0
Jascha Ehrenreich <jascha@jaeh.at>
2014-10-16 01:15:05 +0200

    content json files

---
+++ content/test.js
@@ -0,0 +1,83 @@
+'use strict';
+
+module.exports = {
+		imgDir: '/img/'
+	, firstname: 'Jascha'
+	, lastname: 'Ehrenreich'
+	, image: 'jascha.ehrenreich.png'
+	, jobTitle: 'Webprogrammer and Streetartist'
+	, slogan: 'Magically shifting paradigms'
+	, description: [
+			'i juggle with code since ten,'
+		, 'with cones, balls and rings since twenty'
+		, 'and life juggles with me since around thirty years.'
+		, 'i somehow manage to enjoy most of it.'
+	]
+	, affiliations: {
+			title: 'Organisations'
+		, items: [
+				{
+					name: 'Bitcoiners Without Borders'
+				, url: 'http://bwb.is'
+				, logo: 'bwb.png'
+				, description: "We are a tribe of individuals tired of the destructive trip, workin' on a better Day."
+				, showtitle: true
+				}
+			, {
+					name: 'Metalab'
+				, url: 'https://metalab.at'
+				, logo: 'metalab.png'
+				, description: 'The Metalab is the first Hackerspace in Vienna. I can usually be found in the Lounge.'
+				, showtitle: true
+				}
+			]
+		}
+	, emails: [
+			'jascha@jaeh.at'
+		, 'jascha@bwb.is'
+		]
+	, socialnetworks: [
+			{
+				text: 'github'
+			, logo: 'social/github.png'
+			, url: 'https://github.org/jaeh/'
+			, description: 'octocat says hello.'
+			}
+		, {
+				text: 'keybase.io'
+			, logo: 'social/keybase.png'
+			, url: 'https://keybase.io/jascha'
+			, description: 'my keybase.io profile'
+			}
+		, {
+				text: '@weirdpress'
+			, logo: 'social/twitter.png'
+			, url: 'https://twitter.com/weirdpress'
+			, description: 'my twitter account'
+			}
+		, {
+				text: 'about.me'
+			, logo: 'social/aboutme.png'
+			, url: 'https://about.me/jascha.ehrenreich'
+			, description: 'my about.me page'
+			}
+		, {
+				text: 'suppe. ja. eh.'
+			, logo: 'social/soup.png'
+			, url: 'http://suppe.jaeh.at'
+			, description: 'soup (german: die suppe) is a multi content social blogging network.'
+			}
+		, {
+				text: 'linkedin'
+			, logo: 'social/linkedin.png'
+			, url: 'https://at.linkedin.com/in/jaschaehrenreich'
+			, description: 'linkedin'
+			}
+		, {
+				text: 'facebook'
+			, logo: 'social/facebook.png'
+			, url: 'https://facebook.com/jascha.ehrenreich/'
+			, description: 'you all know the kraken.'
+		}
+	]
+};
